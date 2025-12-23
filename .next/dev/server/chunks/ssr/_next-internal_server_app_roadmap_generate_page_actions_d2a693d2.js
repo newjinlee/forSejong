46f66dd3926b4aeabca66183617c6e21174c9c04
nwjnlee <winniepooh00@naver.com>
2025-12-23 15:30:43 +0900

    Add proper .gitignore and remove .next from tracking

--- .next/dev/server/chunks/ssr/_next-internal_server_app_roadmap_generate_page_actions_d2a693d2.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/roadmap/generate/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_roadmap_generate_page_actions_d2a693d2.js.map
--- .next/dev/static/chunks/app_roadmap_generate_page_tsx_a8c03fea._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_05bf20be._.js",
-  "static/chunks/app_f64f3636._.js",
-  "static/chunks/node_modules_reactflow_dist_style_58d81dac.css"
-],
-    source: "dynamic"
-});
